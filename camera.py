b0e1564e2100f306c289f1878693c45a7a093098
eug-vs <eug-vs@keemail.me>
2020-09-06 21:12:48 +0300

    feat: add maxrange and raystep to cam params

--- camera.py
+++ camera.py
@@ -1,17 +1,17 @@
 from vector import Vector
 from math import sqrt, tan, pi as PI
 
-CAMERA_RANGE = 10
-RAY_STEP = 0.1
 
 # Camera class for scene
 class Camera:
-    def __init__(self, pos, direction, canvas, angle, distance = 1):
+    def __init__(self, pos, direction, canvas, angle, max_range, ray_step = 0.1, distance = 1):
         self.pos = pos
         self.direction = direction
         self.angle = angle
         self.distance = distance
         self.canvas = canvas
+        self.ray_step = ray_step
+        self.max_range = max_range
 
     def screen(self):
         center = self.pos + (self.direction * self.distance)
@@ -39,11 +39,11 @@ class Camera:
         for j in range(self.canvas.height()):
             for i in range(self.canvas.width()):
                 ray_screen_pos = screen["corner"] + screen["h_step"] * i + screen["v_step"] * j
-                ray = (ray_screen_pos - self.pos).normalized() * RAY_STEP
+                ray = (ray_screen_pos - self.pos).normalized() * self.ray_step
 
-                for step in range(int(CAMERA_RANGE / RAY_STEP)):
+                for step in range(int(self.max_range / self.ray_step)):
                     point = self.pos + ray * step
                     if (point in obj):
-                        self.canvas[j][i] = step * RAY_STEP / CAMERA_RANGE
+                        self.canvas[j][i] = step * self.ray_step / self.max_range
                         break
 
--- main.py
+++ main.py
@@ -2,15 +2,13 @@ from math import pi as PI
 from vector import Vector
 from canvas import Canvas
 from camera import Camera
-from scene import Scene
 from sphere import Sphere
 
 
 # Scene
 canvas = Canvas(Vector(40, 20))
-camera = Camera(Vector(3, 3, 0), Vector(0, 0, 1), canvas, PI / 2)
+camera = Camera(Vector(3, 3, 0), Vector(0, 0, 1), canvas, PI / 2, 10)
 sphere = Sphere(Vector(3, 3, 7), 1)
-
 
 
 while(True):
@@ -19,5 +17,4 @@ while(True):
     print(canvas)
     canvas.clear()
     camera.pos += Vector(0.0, 0.0, 0.1)
-
 
