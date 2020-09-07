9262c1a4bc94dc94f4c83ad4265df8d8a1c8f2d7
eug-vs <eug-vs@keemail.me>
2020-09-07 13:57:49 +0300

    feat: create cool scene

--- main.py
+++ main.py
@@ -6,25 +6,32 @@ from sphere import Sphere
 from cuboid import Cuboid
 
 
+# TODO: create camera movement presets, like flying around the scene
+# TODO: scene recording into file + playing
+# TODO: build BVH for objects for better performance in ray tracing
+
 # Scene
 canvas = Canvas(Vector(60, 30))
-camera = Camera(Vector(3, 3, 0), Vector(0, 0, 1), canvas, PI / 2, 6)
+camera = Camera(Vector(3, 3, 0), Vector(0, 0, 1), canvas, PI / 3, 6)
+camera_focus = Vector(3, 3, 5)
+step_mag = 0.3
+
 
 # Objects
-center = Vector(3, 3, 3)
-cube = Cuboid(center, Vector(2, 2, 2))
-sphere = Sphere(center, 1)
-objects = [cube]
+body = Cuboid(Vector(3, 3, 5), Vector(1.2, 3, 1))
+head = Sphere(Vector(3, 4.5, 5), 0.8)
+balls = [Sphere(Vector(x, 1, 5), 1) for x in [2, 4]]
+objects = [body, head] + balls
 
-
-camera_focus = Vector(3, 3, 3)
-step_mag = 0.1
 
 while(True):
+    # Render
     camera.render(objects)
-    camera.direction = (camera_focus - camera.pos).normalized()
-    step = camera.direction.rotate_y(PI / 2).normalized() * step_mag
     print(canvas)
     canvas.clear()
+
+    # Rotate camera
+    camera.direction = (camera_focus - camera.pos).normalized()
+    step = camera.direction.rotate_y(PI / 2).normalized() * step_mag
     camera.pos += step
 
