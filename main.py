5186407604faa63b26373a2fffa6dd8b0a34fbe0
eug-vs <eug-vs@keemail.me>
2020-09-07 20:50:14 +0300

    feat: add Player class to save scenes

--- main.py
+++ main.py
@@ -4,18 +4,18 @@ from canvas import Canvas
 from camera import Camera
 from sphere import Sphere
 from cuboid import Cuboid
+from player import Player
 
+NAME = "scene"
 
 # TODO: create camera movement presets, like flying around the scene
-# TODO: scene recording into file + playing
 # TODO: build BVH for objects for better performance in ray tracing
 
 # Scene
 canvas = Canvas(Vector(60, 30))
 camera = Camera(Vector(3, 3, 0), Vector(0, 0, 1), canvas, PI / 3, 6)
 camera_focus = Vector(3, 3, 5)
-step_mag = 0.3
-
+step_mag = 0.5
 
 # Objects
 body = Cuboid(Vector(3, 3, 5), Vector(1.2, 3, 1))
@@ -24,14 +24,21 @@ balls = [Sphere(Vector(x, 1, 5), 1) for x in [2, 4]]
 objects = [body, head] + balls
 
 
-while(True):
-    # Render
-    camera.render(objects)
-    print(canvas)
-    canvas.clear()
+player = Player(NAME)
+if not player:
+    for i in range(int(2 * PI * (camera.pos - camera_focus).magnitude() / step_mag)):
+        # Render
+        camera.render(objects)
+        print(canvas)
+        player.frame(canvas)
+        canvas.clear()
 
-    # Rotate camera
-    camera.direction = (camera_focus - camera.pos).normalized()
-    step = camera.direction.rotate_y(PI / 2).normalized() * step_mag
-    camera.pos += step
+        # Rotate camera
+        camera.direction = (camera_focus - camera.pos).normalized()
+        step = camera.direction.rotate_y(PI / 2).normalized() * step_mag
+        camera.pos += step
+
+    player.save(NAME)
+
+player.play()
 
---
+++ player.py
@@ -0,0 +1,40 @@
+from time import sleep
+
+
+SEPARATOR = "\n==FRAME==\n"
+
+
+class Player(list):
+    def __init__(self, filename):
+        if filename:
+            self.read(filename)
+
+    def save(self, filename):
+        try:
+            f = open(filename, "w")
+            for frame in self:
+                f.write(frame)
+                f.write(SEPARATOR)
+            f.close()
+        finally:
+            return self
+
+    def read(self, filename):
+        try:
+            f = open(filename, "r")
+            self += f.read().split(SEPARATOR)
+            f.close()
+        finally:
+            return self
+
+    def frame(self, canvas):
+        self.append(str(canvas))
+
+    def play(self, fps = 60):
+        index = 0
+        while True:
+            print(self[index % len(self)], flush=True)
+            index += 1
+            sleep(1 / fps)
+            print("\n" * 100)
+
