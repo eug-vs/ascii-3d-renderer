6516bc66f9354fa73a4ff5ba4298f37fe1a27a00
eug-vs <eug-vs@keemail.me>
2020-09-07 12:51:32 +0300

    feat: determine raystep based on camera range

--- camera.py
+++ camera.py
@@ -4,20 +4,21 @@ from math import sqrt, tan, pi as PI
 
 # Camera class for scene
 class Camera:
-    def __init__(self, pos, direction, canvas, angle, max_range, ray_step = 0.1, distance = 1):
+    def __init__(self, pos, direction, canvas, angle, max_range, distance = 1):
         self.pos = pos
         self.direction = direction
         self.angle = angle
         self.distance = distance
         self.canvas = canvas
-        self.ray_step = ray_step
+        self.ray_step = max_range / len(canvas.chars)
         self.max_range = max_range
+        self.screen_width = distance * tan(angle / 2) * 2
+        self.screen_height = self.screen_width / canvas.aspect_ratio()
 
     def screen(self):
         center = self.pos + (self.direction * self.distance)
-
-        width = self.distance * tan(self.angle / 2) * 2
-        height = width / self.canvas.aspect_ratio()
+        width = self.screen_width
+        height = self.screen_height
 
         h_step = self.direction.rotate_y(PI / 2).normalized() * width / self.canvas.width()
         v_step = Vector(0, -1, 0) * height / self.canvas.height()
@@ -25,25 +26,34 @@ class Camera:
         corner = center - h_step * (self.canvas.width() / 2) - v_step * (self.canvas.height() / 2)
 
         return {
-            "center": center,
-            "width": width,
-            "height": height,
             "v_step": v_step,
             "h_step": h_step,
             "corner": corner,
         }
 
-    def capture(self, obj):
+    def render(self, objects):
         screen = self.screen()
 
+        ray_dir = screen["corner"] - self.pos
         for j in range(self.canvas.height()):
+            ray_dir += screen["v_step"]
             for i in range(self.canvas.width()):
-                ray_screen_pos = screen["corner"] + screen["h_step"] * i + screen["v_step"] * j
-                ray = (ray_screen_pos - self.pos).normalized() * self.ray_step
+                ray_dir += screen["h_step"]
 
+                ray = ray_dir.normalized() * self.ray_step
+
+                point = self.pos
                 for step in range(int(self.max_range / self.ray_step)):
-                    point = self.pos + ray * step
-                    if (point in obj):
-                        self.canvas[j][i] = step * self.ray_step / self.max_range
-                        break
+                    point += ray
+                    for obj in objects:
+                        if point in obj:
+                            dist = step * self.ray_step / self.max_range
+                            self.canvas[j][i] = min(dist, self.canvas[j][i])
+                            break
+                    else:
+                        continue
+                    break
+
+            ray_dir -= screen["h_step"] * self.canvas.width()
+
 
--- main.py
+++ main.py
@@ -3,18 +3,28 @@ from vector import Vector
 from canvas import Canvas
 from camera import Camera
 from sphere import Sphere
+from cuboid import Cuboid
 
 
 # Scene
-canvas = Canvas(Vector(40, 20))
-camera = Camera(Vector(3, 3, 0), Vector(0, 0, 1), canvas, PI / 2, 10)
-sphere = Sphere(Vector(3, 3, 7), 1)
+canvas = Canvas(Vector(60, 30))
+camera = Camera(Vector(3, 3, 0), Vector(0, 0, 1), canvas, PI / 2, 6)
 
+# Objects
+center = Vector(3, 3, 3)
+cube = Cuboid(center, Vector(2, 2, 2))
+sphere = Sphere(center, 1)
+objects = [cube]
+
+
+camera_focus = Vector(3, 3, 3)
+step_mag = 0.1
 
 while(True):
-    camera.capture(sphere)
-    camera.direction = camera.direction.rotate_y( - PI / 360)
+    camera.render(objects)
+    camera.direction = (camera_focus - camera.pos).normalized()
+    step = camera.direction.rotate_y(PI / 2).normalized() * step_mag
     print(canvas)
     canvas.clear()
-    camera.pos += Vector(0.0, 0.0, 0.1)
+    camera.pos += step
 
